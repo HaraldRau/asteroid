f9e95a41741048e3b4f6f8ca3a140e48cb1ab0c9
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2026-03-24 07:59:40 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let ship = game.createSprite(2, 4)
+let asteroid = game.createSprite(randint(0, 4), 0)
+let points = 0
+let game2 = false
+let pause2 = 1000
+let level = 0
+basic.forever(function () {
+	
+})
